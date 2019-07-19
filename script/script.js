c7a4a111e29cb104ce1c0164e06992b113aae666
Abhinav <singh.abhinav6996@gmail.com>
2019-07-19 10:32:20 +0530

    todo UI

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//icon toggle
 var flag = 0;
 
 $(".button").on("click", function() {
@@ -9,15 +10,25 @@ $(".button").on("click", function() {
     flag = 0;
   }
 });
-
+//toggle on
 function on() {
   $("input").addClass("rotate-input");
   $(".button").addClass("button-rotate");
   $(".tools").addClass("tools-rotate");
 }
-
+//toggle off
 function off() {
   $("input").removeClass("rotate-input");
   $(".button").removeClass("button-rotate");
   $(".tools").removeClass("tools-rotate");
+}
+//selectors
+const todoInput = document.querySelector(".todo-input");
+//data array
+const allTodo = [];
+//add item
+function addTodo(event) {
+  if (event.keyCode == 13) {
+    const unittodo = { text: todoInput.nodeValue, isDone: false };
+  }
 }
